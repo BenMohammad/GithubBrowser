8f8d309a15dc9f0aa8190c5259df5d5ccb067205
Ben Mohammad <benmohammad593@gmail.com>
2020-04-24 08:28:28 +0100

    RepoSearchResponse added...

---
+++ app/src/main/java/com/githubbrowser/api/RepoSearchResponse.java
@@ -0,0 +1,53 @@
+package com.githubbrowser.api;
+
+import androidx.annotation.NonNull;
+
+import com.githubbrowser.vo.Repo;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepoSearchResponse {
+
+    @SerializedName("total_count")
+    private int total;
+
+    @SerializedName("items")
+    private List<Repo> items;
+
+    private Integer nextPage;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public List<Repo> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Repo> items) {
+        this.items = items;
+    }
+
+    public Integer getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(Integer nextPage) {
+        this.nextPage = nextPage;
+    }
+
+    @NonNull
+    public List<Integer> getRepoIds() {
+        List<Integer> repoIds = new ArrayList<>();
+        for(Repo repo : items) {
+            repoIds.add(repo.id);
+        }
+        return repoIds;
+    }
+}
